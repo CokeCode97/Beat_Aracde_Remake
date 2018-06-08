876257f1e17e2e004e5bcb31749ae974cf250b7b
박다원 <tapion825@naver.com>
2018-06-08 15:11:52 +0900

    첫등록

---
+++ Beat_Arcade/Assets/Script/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!EventSystem.current.IsPointerOverGameObject())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 touch_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Ray2D touch_ray = new Ray2D(touch_position, Vector2.zero);
+                RaycastHit2D[] touch_hit = Physics2D.RaycastAll(touch_ray.origin, touch_ray.direction);
+
+                for (int i = 0; i < touch_hit.Length; i++)
+                {
+                    if(touch_hit[i].transform.gameObject.CompareTag("Button"))
+                    {
+                        GamePlayManager.note_list[0].BroadcastMessage("Click");
+                    }    
+                }
+            }
+        }
+	}
+
+
+}
---
+++ Beat_Arcade/Assets/Script/note.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events; 
+
+public class note : MonoBehaviour {
+    public float note_speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, -note_speed, 0);
+	}
+
+    void Click()
+    {
+        transform.position = new Vector2(-8.065f, 5f);
+    }
+
+    void Init()
+    {
+        GamePlayManager.note_list.Add(gameObject);
+    }
+}
