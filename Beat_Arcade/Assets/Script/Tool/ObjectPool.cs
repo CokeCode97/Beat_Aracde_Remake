130b36fc639c721d5a1ae1303121b8ee4fec3b74
TaPion825 <jf297@naver.com>
2018-06-11 19:52:37 +0900

    오브젝트풀 구현

---
+++ Beat_Arcade/Assets/Script/Tool/ObjectPool.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : Singleton<ObjectPool> {
+    public List<PooledObject> object_pool = new List<PooledObject>();
+
+    void Awake()
+    {
+        for(int i = 0; i < object_pool.Count; i++)
+        {
+            object_pool[i].Init(transform);
+        }
+    }
+
+    public bool Push_To_Pool(GameObject item, string item_name, Transform parent = null)
+    {
+        PooledObject pool = Get_Pool_Item(item_name);
+        if (pool == null)
+            return false;
+
+        pool.Push_To_Pool(item, parent == null ? transform : parent);
+        return true;
+    }
+
+    public GameObject Pop_From_Pool(string item_name, Transform parent = null)
+    {
+        PooledObject pool = Get_Pool_Item(item_name);
+        if (pool == null)
+            return null;
+
+        return pool.Pop_From_Pool(parent);
+    }
+
+    PooledObject Get_Pool_Item(string item_name)
+    {
+        for(int i = 0; i < object_pool.Count; i++)
+        {
+            if (object_pool[i].pool_item_name.Equals(item_name))
+                return object_pool[i];
+        }
+
+        return null;
+    }
+}
---
+++ Beat_Arcade/Assets/Script/Tool/PooledObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PooledObject
+{
+    public string pool_item_name = string.Empty;
+    public GameObject prefeb = null;
+    public int poolCount = 0;
+
+    [SerializeField]
+    private List<GameObject> pool_list = new List<GameObject>();
+
+    public void Init(Transform parent = null)
+    {
+        for(int i = 0; i < poolCount; i++)
+        {
+            pool_list.Add(CreateItem(parent));
+        }
+    }
+
+    public void Push_To_Pool(GameObject item, Transform parent = null)
+    {
+        item.transform.SetParent(parent);
+        item.SetActive(false);
+        pool_list.Add(item);
+    }
+
+    public GameObject Pop_From_Pool(Transform parent = null)
+    {
+        if (pool_list.Count == 0)
+            pool_list.Add(CreateItem(parent));
+
+        GameObject item = pool_list[0];
+        pool_list.RemoveAt(0);
+
+        return item;
+    }
+
+    private GameObject CreateItem(Transform parent = null)
+    {
+        GameObject item = Object.Instantiate(prefeb) as GameObject;
+        item.name = pool_item_name;
+        item.transform.SetParent(parent);
+        item.SetActive(false);
+
+        return item;
+    }
+}
